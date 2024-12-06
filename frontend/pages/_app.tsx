53374324f52077b1f3e023ce7cf305098bf3049e
Marc_dev <marcramadison006@gmail.com>
2024-12-06 13:50:29 +0100

    change title

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>JDA Diffusion</title>
+        <meta name="description" content="Site à propos de JDA Diffusion, qui offre des services de community manager et de développement web" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
